005fbef2b6dba93ff1ff40877eea6649c557c2f3
Botond Kulcsar <bkulcsar@gmx.com>
2021-09-13 01:06:50 +0300

    trycatch added

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -4,16 +4,20 @@ import './searchMovies.css'
 
 export default function SearchMovies() {
 
-    const [ query, setQuery ] = useState('');
-    const [ movieList, setMovieList ] = useState([]);
+    const [query, setQuery] = useState('');
+    const [movieList, setMovieList] = useState([]);
 
     const getMovies = async () => {
         const apiKey = '65060f34709539c10333a33728fe6197';
         const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
-        const response = await fetch(url);
-        const movies = await response.json();
-        setMovieList(movies.results);
-        console.log(movies);
+        try {
+            const response = await fetch(url);
+            const movies = await response.json();
+            setMovieList(movies.results);
+            console.log(movies);
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
     const handleChange = (event) => {
@@ -26,7 +30,7 @@ export default function SearchMovies() {
         setQuery('')
     }
 
-    
+
 
     return (
         <React.Fragment>
@@ -39,11 +43,11 @@ export default function SearchMovies() {
                     onClick={(event) => event.target.setSelectionRange(0, query.length)}
                     name="movieQuery"
                     id="movieQuery"
-                 />
-                <button disabled = {!query} >Search</button>
+                />
+                <button disabled={!query} >Search</button>
             </form>
             <div className="movie-list">
-                { movieList.filter(m => m.poster_path).map(m => <MovieCard key={m.id} movie={m} />) }
+                {movieList.filter(m => m.poster_path).map(m => <MovieCard key={m.id} movie={m} />)}
             </div>
         </React.Fragment>)
 }
